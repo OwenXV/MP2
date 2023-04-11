52bdd921158dcfbe3a1b2df78fe743073b882c8c
OwenXV <owensanbuenaventura@hotmail.com>
2023-04-11 10:09:39 +0800

    update

---
+++ src/lib/axios.js
@@ -0,0 +1,23 @@
+// import axios from "axios"
+
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://moviesminidatabase.p.rapidapi.com/genres/',
+//   headers: {
+//     'X-RapidAPI-Key': 'dadf28600bmshcc048cb21ffe72cp1a57d9jsn8c87e0ddae98',
+//     'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+
+// export default options
+
