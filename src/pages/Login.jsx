155c17201ae9c3678f21f3223f337933b4869a3f
OwenXV <owensanbuenaventura@hotmail.com>
2023-04-12 04:27:26 +0800

    updated build

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,10 +9,7 @@ function Login  () {
                     <Form className>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="text-white">Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
+                        <Form.Control type="email" placeholder="Enter email" />git 
                     </Form.Group>
                     <Button variant="primary" type="submit"> Login </Button>
                     </Form>
